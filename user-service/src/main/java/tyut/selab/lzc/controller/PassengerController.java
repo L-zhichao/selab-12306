144ba9cadb5d24592b2c4e0a71c1842f8de9b112
lizhichao <2658085067@qq.com>
2024-03-20 18:24:37 +0800

    add userModule

---
+++ user-service/src/main/java/tyut/selab/lzc/controller/PassengerController.java
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tyut.selab.lzc.controller;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.*;
+import tyut.selab.lzc.dto.req.PassengerRemoveReqDTO;
+import tyut.selab.lzc.dto.req.PassengerReqDTO;
+import tyut.selab.lzc.dto.resp.PassengerActualRespDTO;
+import tyut.selab.lzc.dto.resp.PassengerRespDTO;
+import tyut.selab.lzc.service.PassengerService;
+import tyut.selab.lzc.tools.Result;
+import tyut.selab.lzc.tools.Results;
+
+import java.util.List;
+
+/**
+ * 乘车人控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class PassengerController {
+
+    private final PassengerService passengerService;
+
+    /**
+     * 根据用户名查询乘车人列表
+     */
+    @GetMapping("/api/user-service/passenger/query")
+    public Result<List<PassengerRespDTO>> listPassengerQueryByUsername() {
+        return Results.success(passengerService.listPassengerQueryByUsername(UserContext.getUsername()));
+    }
+
+    /**
+     * 根据乘车人 ID 集合查询乘车人列表
+     */
+    @GetMapping("/api/user-service/inner/passenger/actual/query/ids")
+    public Result<List<PassengerActualRespDTO>> listPassengerQueryByIds(@RequestParam("username") String username, @RequestParam("ids") List<Long> ids) {
+        return Results.success(passengerService.listPassengerQueryByIds(username, ids));
+
+    }
+
+    /**
+     * 新增乘车人
+     */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(org.opengoofy.index12306.frameworks.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在新增乘车人，请稍后再试..."
+    )
+    @PostMapping("/api/user-service/passenger/save")
+    public Result<Void> savePassenger(@RequestBody PassengerReqDTO requestParam) {
+        passengerService.savePassenger(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 修改乘车人
+     */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(org.opengoofy.index12306.frameworks.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在修改乘车人，请稍后再试..."
+    )
+    @PostMapping("/api/user-service/passenger/update")
+    public Result<Void> updatePassenger(@RequestBody PassengerReqDTO requestParam) {
+        passengerService.updatePassenger(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 移除乘车人
+     */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(org.opengoofy.index12306.frameworks.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在移除乘车人，请稍后再试..."
+    )
+    @PostMapping("/api/user-service/passenger/remove")
+    public Result<Void> removePassenger(@RequestBody PassengerRemoveReqDTO requestParam) {
+
+        passengerService.removePassenger(requestParam);
+        return Results.success();
+    }
+}
---
+++ user-service/src/main/java/tyut/selab/lzc/controller/UserInfoController.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tyut.selab.lzc.controller;
+
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.*;
+import tyut.selab.lzc.dto.req.UserDeletionReqDTO;
+import tyut.selab.lzc.dto.req.UserRegisterReqDTO;
+import tyut.selab.lzc.dto.req.UserUpdateReqDTO;
+import tyut.selab.lzc.dto.resp.UserQueryActualRespDTO;
+import tyut.selab.lzc.dto.resp.UserQueryRespDTO;
+import tyut.selab.lzc.dto.resp.UserRegisterRespDTO;
+import tyut.selab.lzc.service.UserLoginService;
+import tyut.selab.lzc.service.UserService;
+import tyut.selab.lzc.tools.Result;
+import tyut.selab.lzc.tools.Results;
+
+/**
+ * 用户控制层
+ *
+ * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
+ */
+@RestController
+@RequiredArgsConstructor
+public class UserInfoController {
+
+    private final UserLoginService userLoginService;
+    private final UserService userService;
+
+    /**
+     * 根据用户名查询用户信息
+     */
+    @GetMapping("/api/user-service/query")
+    public Result<UserQueryRespDTO> queryUserByUsername(@RequestParam("username") @NotEmpty String username) {
+        return Results.success(userService.queryUserByUsername(username));
+    }
+
+    /**
+     * 根据用户名查询用户无脱敏信息
+     */
+    @GetMapping("/api/user-service/actual/query")
+    public Result<UserQueryActualRespDTO> queryActualUserByUsername(@RequestParam("username") @NotEmpty String username) {
+        return Results.success(userService.queryActualUserByUsername(username));
+    }
+
+    /**
+     * 检查用户名是否已存在
+     */
+    @GetMapping("/api/user-service/has-username")
+    public Result<Boolean> hasUsername(@RequestParam("username") @NotEmpty String username) {
+        return Results.success(userLoginService.hasUsername(username));
+    }
+
+    /**
+     * 注册用户
+     */
+    @PostMapping("/api/user-service/register")
+    public Result<UserRegisterRespDTO> register(@RequestBody @Valid UserRegisterReqDTO requestParam) {
+        return Results.success(userLoginService.register(requestParam));
+    }
+
+    /**
+     * 修改用户
+     */
+    @PostMapping("/api/user-service/update")
+    public Result<Void> update(@RequestBody @Valid UserUpdateReqDTO requestParam) {
+        userService.update(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 注销用户
+     */
+    @PostMapping("/api/user-service/deletion")
+    public Result<Void> deletion(@RequestBody @Valid UserDeletionReqDTO requestParam) {
+        userLoginService.deletion(requestParam);
+        return Results.success();
+    }
+}
